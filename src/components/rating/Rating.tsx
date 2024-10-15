0dc0383e1bf5897a80645c849505e2b0c17eed0e
hausners <stefan.hausner@camline.com>
2024-10-15 13:26:47 +0200

    star rating

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -9,11 +9,12 @@ export default function Rating() {
   
   const handleStarClick = (event: React.MouseEvent<HTMLDivElement>) => {
     const target = event.currentTarget;
-    const selected = target.querySelector(`${styles.selected}`);
+    const selected = target.querySelector(`.${styles.selected}`);
+    console.log("selected " + target + selected)
     if (selected) {
-      selected.classList.add(`${styles.isAnimated}`, `${styles.pulse}`);
+      selected.classList.add(styles.isAnimated, styles.pulse);
       setTimeout(() => {
-        selected.classList.remove(`${styles.isAnimated}`, `${styles.pulse}`);
+        selected.classList.remove(styles.isAnimated, styles.pulse);
       }, 1000);
     }
     setStarClicked(true);
@@ -24,16 +25,19 @@ export default function Rating() {
     let sibling = parent.previousElementSibling as HTMLElement
     
     while (sibling) {
-      sibling.classList.add(`${styles.animate}`);
-      sibling.querySelector('span')?.classList.add(`${styles.starColour}`);
+      sibling.classList.add(styles.animate);
+      const spans = sibling.querySelectorAll('span');
+      
+      spans.forEach(span => {
+        span.classList.add(styles.starColour); // add the class from both spans
+      });
+      
       sibling = sibling.previousElementSibling as HTMLElement;
     }
     //sibling = target.nextElementSibling as HTMLElement;
     sibling = parent.nextElementSibling as HTMLElement;
     while (sibling) {
-      sibling.classList.remove(`${styles.animate}`);
-      //sibling.querySelector('span')?.classList.remove(`${styles.starColour}`);
-      //sibling.querySelector('span')?.classList.remove(`${styles.starColour}`);
+      sibling.classList.remove(styles.animate);
       const spans = sibling.querySelectorAll('span');
       
       spans.forEach(span => {
@@ -42,19 +46,17 @@ export default function Rating() {
       
       sibling = sibling.nextElementSibling as HTMLElement;
     }
-    if (sibling == null){
-      
-    }
 
   }
 
-  const handleFullClick = (event: React.MouseEvent<HTMLSpanElement>) => {
+  const handleFullClick = (event: React.MouseEvent<HTMLDivElement>) => {
     if (starClicked) {
       console.log("full " + event.currentTarget)
       setFullStarState(event.currentTarget);
       const value = parseInt(event.currentTarget.dataset.value || "0");
       setRating(value);
       console.log("full " + rating)
+      handleStarClick()
     }
   };
 
@@ -68,10 +70,10 @@ export default function Rating() {
     }
   };
 
-  const handleFullHover = (event: React.MouseEvent<HTMLSpanElement>) => {
+  const handleFullHover = (event: React.MouseEvent<HTMLDivElement>) => {
     if (!starClicked) {
-      setFullStarState(event.currentTarget);
-    }
+      setFullStarState(event.currentTarget.parentElement as HTMLSpanElement);
+    } 
   };
 
   const handleHalfHover = (event: React.MouseEvent<HTMLSpanElement>) => {
@@ -81,37 +83,45 @@ export default function Rating() {
   };
 
   const setHalfStarState = (target: HTMLSpanElement) => {
-    target.classList.add(`${styles.starColour}`);
+    console.log("in halfstate" + target)
+    target.classList.add(styles.starColour);
     const prevSibling = target.previousElementSibling as HTMLSpanElement;
-    prevSibling?.classList.add(`${styles.starColour}`);
+    prevSibling?.classList.add(styles.starColour);
     updateStarState(target);
   }
 
   const setFullStarState = (target: HTMLSpanElement) => {
-    target.classList.add(`${styles.starColour}`);
-    target.parentElement?.classList.add(`${styles.animate}`);
+    console.log("in fullstate" + target)
+    target.classList.add(styles.starColour);
+    const selected = target.parentElement?.querySelector(`.${styles.selected}`) as HTMLSpanElement;
+    selected.classList.add(styles.starColour);
+    target.parentElement?.classList.add(styles.animate);
     const prevSibling = target.previousElementSibling as HTMLSpanElement;
-    prevSibling?.classList.remove(`${styles.starColour}`);
+    prevSibling?.classList.remove(styles.starColour);
     updateStarState(target);
   }
+
   
   return (
     <div className={styles.rating}>
-      {[1, 2, 3, 4, 5].map((num) => (
-        <div className={styles.star} key={num} onClick={handleStarClick}>
+      {[0,1, 2, 3, 4, 5].map((num) => (
+        <div className={num === 0 ? styles.starHidden : styles.star} key={num} onClick={handleStarClick}>
           <span
             className={styles.full}
-            data-value={num}
-            onClick={handleFullClick}
-            onMouseOver={handleFullHover}
-          ></span>
+          >
+            <div className={styles.fullArea}
+              data-value={num}
+              onClick={handleFullClick}
+              onMouseOver={handleFullHover}/>
+          </span>
           <span
             className={styles.half}
             data-value={num - 0.5}
             onClick={handleHalfClick}
             onMouseOver={handleHalfHover}
-          ></span>
-          <span className={styles.selected}></span>
+          />
+          
+          <span className={styles.selected}/>
         </div>
       ))}
 
